cba59adf8b1ce237c6eb15f7ae9350df089b4405
leif-behrens <info@leifbehrens.de>
2020-03-14 14:57:42 +0100

    Init git

---
+++ Client.py
@@ -0,0 +1,423 @@
+import socket
+import pickle
+import sqlite3
+import json
+import threading
+import os
+import sys
+import time
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+class Client(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setMinimumSize(600, 400)
+        self.setWindowTitle("Quiz")
+        self.width = self.frameGeometry().width()
+        self.height = self.frameGeometry().height()
+        self.init_layouts()
+
+        
+
+        self.connected = False
+        self.authenticated = False
+
+        if os.path.exists("Config/clientsettings.json"):
+            with open("Config/clientsettings.json", "r+") as f:
+                try:
+                    self.settings = json.load(f)
+                    
+                    self.le_ip.setText(str(self.settings.get("serversettings").get("ip")))
+                    self.le_port.setText(str(self.settings.get("serversettings").get("port")))
+                    self.le_username.setText(str(self.settings.get("usersettings").get("username")))
+                    self.le_password.setText(str(self.settings.get("usersettings").get("password")))
+                    self.cb_autologin.setChecked(self.settings.get("generalsettings").get("autologin"))
+                    self.cb_remember_data.setChecked(self.settings.get("generalsettings").get("rememberdata"))
+
+                except Exception as e:
+                    self.settings = {}
+                    print(e)
+        else:
+            self.settings = {}
+
+        self.timer_connect = QTimer()
+        self.timer_connect.timeout.connect(self.connect_to_server)
+        self.timer_connect.start(5000)
+            
+
+    def connect_to_server(self):
+        if not self.connected:
+            if self.settings:
+                if self.settings.get("generalsettings").get("autologin"):
+                    try:
+                        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        self.client.connect((self.settings.get("serversettings").get("ip"), self.settings.get("serversettings").get("port")))
+                        self.connected = True
+
+                        self.authentication()
+                    except Exception as e:
+                        print(e)
+    
+    def authentication(self):
+        if self.connected:
+            if self.settings:
+                try:
+                    credentials = [self.settings.get("usersettings").get("username"), self.settings.get("usersettings").get("password")]
+                    pickled = pickle.dumps(credentials)
+                    
+                    try:
+                        self.client.send(pickled)
+                        self.authenticated = pickle.loads(self.client.recv(1024))
+                        print(self.authenticated)
+                    except Exception as e:
+                        self.connected = False
+                        print(e)
+
+                except Exception as e:
+                    print(e)
+
+    def init_layouts(self):
+        self.home_layout()
+        self.new_quiz_layout()
+        self.highscore_layout()
+        self.new_question_layout()
+        self.edit_question_layout()
+        self.login_layout()
+
+        self.show()
+
+    def home_layout(self):
+        self.home_widget = QWidget(self)
+        self.home_widget.resize(self.width, self.height-25)
+        vbox = QVBoxLayout()
+
+        hbox0 = QHBoxLayout()
+        lb_title = QLabel("Quiz")
+        lb_title.setFont(QFont("Times New Roman", 40, QFont.Bold))
+        lb_title.setAlignment(Qt.AlignCenter)
+        hbox0.addWidget(lb_title)
+
+        hbox_space = QHBoxLayout()
+        hbox_space.addWidget(QLabel())
+
+        hbox1 = QHBoxLayout()
+        btn_start_quiz = QPushButton("Neues Quiz")
+        btn_start_quiz.setFont(QFont("Times New Roman", 25, QFont.Cursive))
+        btn_start_quiz.clicked.connect(self.show_new_quiz)
+        hbox1.addWidget(btn_start_quiz)
+        hbox1.addStretch()
+
+        hbox2 = QHBoxLayout()
+        btn_show_highscore = QPushButton("Highscoreliste")
+        btn_show_highscore.setFont(QFont("Times New Roman", 25, QFont.Cursive))
+        btn_show_highscore.clicked.connect(self.show_highscore)
+        hbox2.addWidget(btn_show_highscore)
+        hbox2.addStretch()
+
+        hbox3 = QHBoxLayout()
+        btn_new_question = QPushButton("Neue Frage erstellen")
+        btn_new_question.setFont(QFont("Times New Roman", 25, QFont.Cursive))
+        btn_new_question.clicked.connect(self.show_new_question)
+        hbox3.addWidget(btn_new_question)
+        hbox3.addStretch()
+
+        hbox4 = QHBoxLayout()
+        btn_edit_delete_question = QPushButton("Frage bearbeiten/löschen")
+        btn_edit_delete_question.setFont(QFont("Times New Roman", 25, QFont.Cursive))
+        btn_edit_delete_question.clicked.connect(self.show_edit_question)
+        hbox4.addWidget(btn_edit_delete_question)
+        hbox4.addStretch()
+
+        hbox5 = QHBoxLayout()
+        btn_login = QPushButton("Login")
+        btn_login.setFont(QFont("Times New Roman", 25, QFont.Cursive))
+        btn_login.clicked.connect(self.show_login)
+        hbox5.addWidget(btn_login)
+        hbox5.addStretch()
+
+        vbox.addLayout(hbox0)
+        vbox.addLayout(hbox_space)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+        vbox.addLayout(hbox5)
+        vbox.addStretch()
+
+        self.home_widget.setLayout(vbox)
+
+    def new_quiz_layout(self):
+        self.new_quiz_widget = QWidget(self)
+
+        self.new_quiz_widget.hide()
+
+    def highscore_layout(self):
+        self.highscore_widget = QWidget(self)
+
+        self.highscore_widget.hide()
+
+    def new_question_layout(self):
+        self.new_question_widget = QWidget(self)
+        self.new_question_widget.resize(self.width, self.height-25)
+        
+        vbox = QVBoxLayout()
+
+        hbox0 = QHBoxLayout()
+        lb_question = QLabel("Frage")
+        lb_question.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+        self.le_question = QLineEdit()
+        self.le_question.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+
+        hbox0.addWidget(lb_question)
+        hbox0.addWidget(self.le_question)
+
+        hbox_space = QHBoxLayout()
+        hbox_space.addWidget(QLabel())
+
+        hbox1 = QHBoxLayout()
+        lb_correct_answer = QLabel("Richtige Antwort  ")
+        lb_correct_answer.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        self.le_correct_answer = QLineEdit()
+        self.le_correct_answer.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+
+        lb_wrong_answer_1 = QLabel("Falsche Antwort 1")
+        lb_wrong_answer_1.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        self.le_wrong_answer_1 = QLineEdit()
+        self.le_wrong_answer_1.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        
+        hbox1.addWidget(lb_correct_answer)
+        hbox1.addWidget(self.le_correct_answer)
+        hbox1.addStretch()
+        hbox1.addWidget(lb_wrong_answer_1)
+        hbox1.addWidget(self.le_wrong_answer_1)
+
+        hbox2 = QHBoxLayout()
+        
+        lb_wrong_answer_2 = QLabel("Falsche Antwort 2")
+        lb_wrong_answer_2.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        self.le_wrong_answer_2 = QLineEdit()
+        self.le_wrong_answer_2.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+
+        lb_wrong_answer_3 = QLabel("Falsche Antwort 3")
+        lb_wrong_answer_3.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        self.le_wrong_answer_3 = QLineEdit()
+        self.le_wrong_answer_3.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+
+        hbox2.addWidget(lb_wrong_answer_2)
+        hbox2.addWidget(self.le_wrong_answer_2)  
+        hbox2.addStretch()      
+        hbox2.addWidget(lb_wrong_answer_3)
+        hbox2.addWidget(self.le_wrong_answer_3)
+
+        hbox3 = QHBoxLayout()
+
+        btn_save = QPushButton("Speichern")
+        btn_save.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+        btn_cancel = QPushButton("Abbrechen")
+        btn_cancel.clicked.connect(self.show_home)
+        btn_cancel.setFont(QFont("Times New Roman", 12, QFont.Cursive))
+    	
+        hbox3.addStretch()
+        hbox3.addWidget(btn_save)
+        hbox3.addWidget(btn_cancel)
+
+        vbox.addLayout(hbox0)
+        vbox.addLayout(hbox_space)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addStretch()
+        vbox.addLayout(hbox3)
+        
+        self.new_question_widget.setLayout(vbox)
+        self.new_question_widget.hide()
+    
+    def edit_question_layout(self):
+        self.edit_question_widget = QWidget(self)
+
+        self.edit_question_widget.hide()
+
+    def login_layout(self):
+        self.login_widget = QWidget(self)
+
+        vbox = QVBoxLayout()
+        
+
+        hbox0 = QHBoxLayout()
+
+        lb_login = QLabel("Login")
+        lb_login.setFont(QFont("Times New Roman", 40, QFont.Bold))
+        lb_login.setAlignment(Qt.AlignCenter)
+        hbox0.addWidget(lb_login)
+
+        hbox_space0 = QHBoxLayout()
+        hbox_space0.addWidget(QLabel())
+
+
+        hbox1 = QHBoxLayout()
+        
+        lb_server = QLabel("Server IP")
+        lb_server.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+        self.le_ip = QLineEdit()
+
+        hbox1.addWidget(lb_server)
+        hbox1.addWidget(self.le_ip)
+        hbox1.addStretch()
+        
+
+        hbox2 = QHBoxLayout()
+
+        lb_port = QLabel("Port")
+        lb_port.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+        self.le_port = QLineEdit()
+
+        hbox2.addWidget(lb_port)
+        hbox2.addWidget(self.le_port)
+        hbox2.addStretch(1)
+
+
+        hbox_space1 = QHBoxLayout()
+        hbox_space1.addWidget(QLabel())
+
+
+        hbox3 = QHBoxLayout()
+
+        lb_username = QLabel("Benutzername")
+        lb_username.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+        self.le_username = QLineEdit()
+
+        hbox3.addWidget(lb_username)
+        hbox3.addWidget(self.le_username)
+        hbox3.addStretch()
+
+
+        hbox4 = QHBoxLayout()
+
+        lb_password = QLabel("Passwort")
+        lb_password.setFont(QFont("Times New Roman", 15, QFont.Cursive))
+        self.le_password = QLineEdit()
+        self.le_password.setEchoMode(QLineEdit.Password)
+
+        hbox4.addWidget(lb_password)
+        hbox4.addWidget(self.le_password)
+        hbox4.addStretch()
+
+
+        hbox5 = QHBoxLayout()
+
+        self.cb_remember_data = QCheckBox("Daten merken")
+        self.cb_autologin = QCheckBox("Automatischer Login")
+        
+        hbox5.addWidget(self.cb_remember_data)
+        hbox5.addWidget(self.cb_autologin)
+        hbox5.addStretch()
+
+
+        hbox6 = QHBoxLayout()
+        
+        btn_save = QPushButton("Speichern und Ausführen")
+        #btn_save.clicked.connect()
+        btn_cancel = QPushButton("Abbrechen")
+        btn_cancel.clicked.connect(self.show_home)
+        hbox6.addStretch()
+        hbox6.addWidget(btn_save)
+        hbox6.addWidget(btn_cancel)
+
+
+        vbox.addLayout(hbox0)
+        vbox.addLayout(hbox_space0)
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox_space1)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+        vbox.addLayout(hbox5)
+
+        vbox.addStretch()
+
+        vbox.addLayout(hbox6)
+
+        self.login_widget.setLayout(vbox)
+
+        self.login_widget.hide()
+    
+    def show_home(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+        
+        self.home_widget.show()
+
+    def show_new_quiz(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+        
+        self.new_quiz_widget.show()
+
+    def show_highscore(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+
+        self.highscore_widget.show()
+
+    def show_new_question(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+
+        self.new_question_widget.show()
+
+    def show_edit_question(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+
+        self.edit_question_widget.show()
+
+    def show_login(self):
+        self.home_widget.hide()
+        self.new_quiz_widget.hide()
+        self.highscore_widget.hide()
+        self.new_question_widget.hide()
+        self.edit_question_widget.hide()
+        self.login_widget.hide()
+
+        self.login_widget.show()
+
+    def resizeEvent(self, event):        
+        self.width = self.frameGeometry().width()
+        self.height = self.frameGeometry().height()
+        
+        self.home_widget.resize(self.width, self.height-25)
+        self.new_question_widget.resize(self.width, self.height-25)
+        self.highscore_widget.resize(self.width, self.height-25)
+        self.new_question_widget.resize(self.width, self.height-25)
+        self.edit_question_widget.resize(self.width, self.height-25)
+        self.login_widget.resize(self.width, self.height-25)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    client = Client()
+    sys.exit(app.exec_())
---
+++ server.py
@@ -0,0 +1,244 @@
+import socket
+import pickle
+import sqlite3
+import json
+import threading
+import os
+import hashlib
+
+# Decorator
+def check_permission(username, password):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            conn = sqlite3.connect("Database/user.db")
+            cur = conn.cursor()
+            
+            try:
+                cur.execute("SELECT password FROM user WHERE username = ?", (username,))
+                data = cur.fetchone()
+                
+                if data is None:
+                    return False, "Username does not exists"
+                else:
+                    if data != password:
+                        return False, "Password is not correct"
+
+                    else:
+                        return_value = function(*args, **kwargs)
+                        return return_value
+
+            except Exception as e:
+                return False, e
+        return wrapper
+    return decorator
+
+
+class Server:
+    def __init__(self):
+        
+        if os.path.exists("Config/serversettings.json"):
+            with open("Config/serversettings.json") as f:
+                config = json.load(f)
+            
+            self.ip = config.get("ip")
+            self.port = config.get("port")
+            
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.bind((self.ip, self.port))
+                self.sock.listen(5)
+
+            except Exception as e:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.bind(("localhost", 50000))
+                self.sock.listen(5)
+        else:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.bind(("localhost", 50000))
+            self.sock.listen(5)
+
+    def run(self):
+        while True:
+            client, addr = self.sock.accept()                
+            threading._start_new_thread(self.new_client_connection, (client, addr))
+
+    def new_client_connection(self, client, addr):
+        print(f"Neue eingehende Verbindung von {addr[0]}:{addr[1]}")
+        data = client.recv(1024)
+        authentication = pickle.loads(data)
+
+        access = self.check_credentials(*authentication)
+
+        client.send(pickle.dumps(access))
+
+        if not authentication:
+            client.close()
+
+    def check_credentials(self, username, password):
+        person = PersonDatabase("Database/user.db")
+
+        hashed_password = hashlib.sha256()
+        hashed_password.update(password.encode("utf-8"))
+
+        access = person.check_access(username, hashed_password.hexdigest())
+        return access
+
+
+class QuizDatabase:
+    def __init__(self, database, username=None, password=None):
+        self.username = username
+        self.password = password
+
+        person = PersonDatabase("Database/users.db")
+        self.permission = person.check_access(username, password)
+
+        # Initialize SQLite3-Connection and create cursor
+        self._conn = sqlite3.connect(database)
+        self._cur = self._conn.cursor()
+
+        # Create Table if not exist
+        query = """
+        CREATE TABLE IF NOT EXISTS quiz (
+            quiz_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT,
+            wrong_answer_1 TEXT,
+            wrong_answer_2 TEXT,
+            wrong_answer_3 TEXT,
+            correct_answer TEXT,
+            category TEXT,
+            difficulty_level INTEGER,
+            author TEXT,
+            editor TEXT,
+            timestamp_creation INTEGER,
+            timestamp_lastchange INTEGER
+        )
+        """
+
+        self._cur.execute(query)
+        self._conn.commit()
+
+    def new_question(self, question, wrong_answer_1, wrong_answer_2, wrong_answer_3, correct_answer, category, difficulty_level):
+        if self.permission:
+            query = """
+            INSERT INTO quiz (
+                question,
+                wrong_answer_1,
+                wrong_answer_2,
+                wrong_answer_3,
+                correct_answer,
+                category,
+                difficulty_level,
+                author,
+                editor,
+                timestamp_creation,
+                timestamp_lastchange
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """
+            
+            try:
+                self._cur.execute(query, (question, wrong_answer_1, wrong_answer_2, wrong_answer_3, correct_answer, category, difficulty_level, self.username, self.username, int(time.time()), int(time.time)))
+                self._conn.commit()
+                
+                return True, ""
+
+            except Exception as e:
+                return False, e
+    
+    def change_question(self, primarykey, column, new_value):
+        if self.permission:
+            query = f"""
+            UPDATE quiz 
+            SET {column} = ?, editor = ?, timestamp_lastchange = ?
+            WHERE quiz_id = ?
+            """
+
+            try:
+                self._cur.execute(query, (new_value, self.username, int(time.time()), primarykey))
+                self._conn.commit()
+                return True, ""
+
+            except Exception as e:
+                return False, e
+
+    def delete_question(self, primarykey):
+        if self.permission:
+            query = f"""
+            DELETE FROM quiz WHERE quiz_id = ?
+            """
+
+            try:
+                self._cur.execute(query, (primarykey))
+                self._conn.commit()
+                return True, ""
+            
+            except Exception as e:
+                return False, e
+    
+
+class PersonDatabase:
+    def __init__(self, database, admin=False):
+        self._conn = sqlite3.connect(database)
+        self._cur = self._conn.cursor()
+
+        self.admin = admin
+
+        query = """
+        CREATE TABLE IF NOT EXISTS user (
+            username TEXT PRIMARY KEY,
+            password TEXT,
+            f_name TEXT,
+            l_name TEXT,
+            email TEXT,
+            admin BOOL
+        )
+        """
+
+        self._cur.execute(query)
+        self._conn.commit()
+
+    def new_user(self, username, password_hash, f_name, l_name, email, admin=False):
+        # Check if username exists
+        exists = self._cur.execute("SELECT * FROM user WHERE username = ?", (username,))
+
+        if exists is None:
+            query = """
+            INSERT INTO user (username, password, f_name, l_name, email, admin) VALUES (
+                ?, ?, ?, ?, ?, ?
+            )
+            """
+            try:
+                self._cur.execute(query, (username, password_hash, f_name, l_name, admin))
+                self._conn.commit()
+                return True, ""
+
+            except Exception as e:
+                return False, e
+        
+        else:
+            return False, "Username already exists"
+    
+    def check_access(self, username, hashed_password):
+        query = """
+        SELECT password FROM user WHERE username = ?
+        """
+
+        self._cur.execute(query, (username,))
+
+        data = self._cur.fetchone()
+
+        if data is None:
+            return False
+
+        else:
+            if hashed_password == data:
+                return True
+            else:
+                return False
+
+
+if __name__ == "__main__":
+    s = Server()
+    s.run()
+    # d = QuizDatabase("Database/quiz.db")
+    # print(d.new_question("owqnf", "ik", "omo", "oikm", "ownergfiou", "aaa", 13))
+    # d.change_question(1, "question", "qssdfsf")
